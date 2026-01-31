8253bc5cc3e977ebfbe1eafd43d0427ade3b3829
Said Meza <176p0393@itspozarica.edu.mx>
2026-01-31 15:16:34 -0600

    type data

---
+++ tipos-de-dato.js
@@ -0,0 +1,12 @@
+// Ejemplo dado
+console.log(typeof 42); // number
+
+// Valores solicitados
+console.log(typeof 42);            // number
+console.log(typeof 'Veinticinco'); // string
+console.log(typeof -666);          // number
+console.log(typeof true);          // boolean
+console.log(typeof 0);             // number
+console.log(typeof '');            // string
+console.log(typeof null);          // object (esto es una rareza histórica de JS)
+console.log(typeof undefined);     // undefined
